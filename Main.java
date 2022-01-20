847e21179a43094066e5e39dd1f2f8061a394385
e1b18049@oit.ac.jp <e1b18049@st.oit.ac.jp>
2022-01-20 13:31:11 +0900

    test2

--- Main.java
+++ Main.java
@@ -3,36 +3,35 @@
 
 public class Main {
     public static void main(String[] args) {
-        //課題2
-        //名前,年齢をPersonクラスを使ったものに変更する
-        String name = "KateJones";
-        int age = 20;
+        // 課題2
+        // 名前,年齢をPersonクラスを使ったものに変更する
+        Person person = new Person("Kate", "Jones", 20);
 
-        //課題3
-        //身長,体重,BMIをHealthクラスで定義および計算するように変更する
+        // 課題3
+        // 身長,体重,BMIをHealthクラスで定義および計算するように変更する
         double height = 1.735;
         double weight = 67.0;
         double bmi = weight / height / height;
 
-        //課題4
-        //名前,年齢,身長,体重を入力する機能
-        //Scanner scanner = new Scanner(System.in);
+        // 課題4
+        // 名前,年齢,身長,体重を入力する機能
+        // Scanner scanner = new Scanner(System.in);
         //
-        //scanner.close();
+        // scanner.close();
 
-        //課題1
-        //名前,年齢,身長,体重,BMIの表示
-        System.out.println("私の名前は"+name+"です");
-        System.out.println("年齢は"+age+"歳です");
-        System.out.println("身長は"+height+"mです");
-        System.out.println("体重は"+weight+"kgです");
-        System.out.println("体重は"+bmi+"です");
+        // 課題1
+        // 名前,年齢,身長,体重,BMIの表示
+        System.out.println("私の名前は" + person.name + "です");
+        System.out.println("年齢は" + person.age + "歳です");
+        System.out.println("身長は" + height + "mです");
+        System.out.println("体重は" + weight + "kgです");
+        System.out.println("体重は" + bmi + "です");
 
     }
-    //課題3
-    //課題1の名前,年齢,身長,体重,BMIの表示機能をprintDataメソッドに移動
-    //BMIが標準値を満たしているかの表示
-    //public static void printData(Person person) {
+    // 課題3
+    // 課題1の名前,年齢,身長,体重,BMIの表示機能をprintDataメソッドに移動
+    // BMIが標準値を満たしているかの表示
+    // public static void printData(Person person) {
     //
-    //}
+    // }
 }
